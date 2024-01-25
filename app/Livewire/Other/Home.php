60509ba641124481956060b9d96389df174216ea
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-01-25 10:35:06 +0200

    Added test properties to home Livewire component

--- app/Livewire/Other/Home.php
+++ app/Livewire/Other/Home.php
@@ -6,6 +6,21 @@ use Livewire\Component;
 
 class Home extends Component
 {
+    public $heroSlides;
+    public $programCards;
+    public $aboutContent;
+
+    public function mount()
+    {
+        try {
+            $this->heroSlides = \App\Models\Home\HeroContent::all();
+            $this->programCards = \App\Models\Home\ProgramsContent::all();
+            $this->aboutContent = \App\Models\Home\AboutContent::all();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
     #[Layout('layouts.homeLayout')]
     public function render()
     {
