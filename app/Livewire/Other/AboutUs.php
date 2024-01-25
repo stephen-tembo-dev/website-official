49f5a393d9be0f64d7671183edf5267dbf2d6261
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-01-25 23:06:45 +0200

    Started dynamically populating about page view

--- app/Livewire/Other/AboutUs.php
+++ app/Livewire/Other/AboutUs.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\Other;
 
+use App\Models\About\AboutBanner;
+use App\Models\About\AboutInfoList;
+use App\Models\About\AboutMainContent;
 use Livewire\Component;
 
 class AboutUs extends Component
 {
+    public $banner = null;
+    public $mainContent = null;
+    public $infoList = null;
+
+    public function mount()
+    {
+        $this->banner = AboutBanner::latest('created_at')->first();
+        $this->mainContent = AboutMainContent::latest('created_at')->first();
+        $this->infoList = AboutInfoList::latest('created_at')->limit(4)->get();
+    }
+
     public function render()
     {
         return view('livewire.other.about-us');
