24b081de3061440bd71ba62ce87a21940fd14f38
Stephen Tembo <stephen187dev@gmail.com>
2024-01-26 11:41:41 +0200

    about editor form enhancements

--- resources/views/livewire/about-editor/edit-info-list-content.blade.php
+++ resources/views/livewire/about-editor/edit-info-list-content.blade.php
@@ -5,51 +5,53 @@
 
         <div class="row">
             <div class="col m8 s12 white">
-
-                <form wire:submit.prevent="processInfo">
-                    <div>
-                        <div class="row">
-                            <div class="input-field col m12 s12">
-                                <input wire:model="listContentArr.title" id="title" type="text" class="validate"
-                                    maxlength="70">
-                                <label class="active" for="title">Title</label>
-                                @error('listContentArr.title')
+                <div class="custom-form z-depth-1">
+                    <form wire:submit.prevent="processInfo">
+                        <div>
+                            <div class="row">
+                                <div class="input-field col m12 s12">
+                                    <input wire:model="listContentArr.title" id="title" type="text" class="validate"
+                                        maxlength="70">
+                                    <label class="active" for="title">Title</label>
+                                    @error('listContentArr.title')
                                     <span class="red-text">{{ $message }}</span>
-                                @enderror
+                                    @enderror
+                                </div>
+
+                                <div class="input-field col s12">
+                                    <textarea wire:model="listContentArr.text" maxlength="2048" id="text"
+                                        class="materialize-textarea"></textarea>
+                                    <label for="text">Text</label>
+                                    @error('listContentArr.text')
+                                    <span class="red-text">{{ $message }}</span>
+                                    @enderror
+                                </div>
                             </div>
 
-                            <div class="input-field col s12">
-                                <textarea wire:model="listContentArr.text" maxlength="2048" id="text" class="materialize-textarea"></textarea>
-                                <label for="text">Text</label>
-                                @error('listContentArr.text')
-                                    <span class="red-text">{{ $message }}</span>
-                                @enderror
-                            </div>
+                            <button style="border-radius: 8px;" class="btn btn-small waves-effect waves-light black"
+                                type="submit" name="action">
+                                Update List Content
+                            </button>
                         </div>
-
-                        <button style="border-radius: 8px;" class="btn btn-small waves-effect waves-light black"
-                            type="submit" name="action">
-                            Update List Content
-                        </button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 @script
-    <script>
-        $wire.on('list-content-updated', () => {
-            M.toast({
-                html: 'updated successfully'
-            })
-        });
+<script>
+$wire.on('list-content-updated', () => {
+    M.toast({
+        html: 'updated successfully'
+    })
+});
 
-        $wire.on('list-content-update-failed', () => {
-            M.toast({
-                html: 'update unsuccessful'
-            })
-        });
-    </script>
+$wire.on('list-content-update-failed', () => {
+    M.toast({
+        html: 'update unsuccessful'
+    })
+});
+</script>
 @endscript
--- resources/views/livewire/about-editor/edit-main-content.blade.php
+++ resources/views/livewire/about-editor/edit-main-content.blade.php
@@ -5,51 +5,53 @@
 
         <div class="row">
             <div class="col m8 s12 white">
-
-                <form wire:submit.prevent="processInfo">
-                    <div>
-                        <div class="row">
-                            <div class="input-field col m12 s12">
-                                <input wire:model="mainContentArr.title" id="title" type="text" class="validate"
-                                    maxlength="70">
-                                <label class="active" for="title">Title</label>
-                                @error('mainContentArr.title')
+                <div class="custom-form z-depth-1">
+                    <form wire:submit.prevent="processInfo">
+                        <div>
+                            <div class="row">
+                                <div class="input-field col m12 s12">
+                                    <input wire:model="mainContentArr.title" id="title" type="text" class="validate"
+                                        maxlength="70">
+                                    <label class="active" for="title">Title</label>
+                                    @error('mainContentArr.title')
                                     <span class="red-text">{{ $message }}</span>
-                                @enderror
+                                    @enderror
+                                </div>
+
+                                <div class="input-field col s12">
+                                    <textarea wire:model="mainContentArr.text" maxlength="2048" id="textarea2"
+                                        class="materialize-textarea"></textarea>
+                                    <label for="textarea2">Text</label>
+                                    @error('mainContentArr.text')
+                                    <span class="red-text">{{ $message }}</span>
+                                    @enderror
+                                </div>
                             </div>
 
-                            <div class="input-field col s12">
-                                <textarea wire:model="mainContentArr.text" maxlength="2048" id="textarea2" class="materialize-textarea"></textarea>
-                                <label for="textarea2">Text</label>
-                                @error('mainContentArr.text')
-                                    <span class="red-text">{{ $message }}</span>
-                                @enderror
-                            </div>
+                            <button style="border-radius: 8px;" class="btn btn-small waves-effect waves-light black"
+                                type="submit" name="action">
+                                Update Main Content
+                            </button>
                         </div>
-
-                        <button style="border-radius: 8px;" class="btn btn-small waves-effect waves-light black"
-                            type="submit" name="action">
-                            Update Main Content
-                        </button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 @script
-    <script>
-        $wire.on('main-content-updated', () => {
-            M.toast({
-                html: 'updated successfully'
-            })
-        });
+<script>
+$wire.on('main-content-updated', () => {
+    M.toast({
+        html: 'updated successfully'
+    })
+});
 
-        $wire.on('main-content-update-failed', () => {
-            M.toast({
-                html: 'update unsuccessful'
-            })
-        });
-    </script>
+$wire.on('main-content-update-failed', () => {
+    M.toast({
+        html: 'update unsuccessful'
+    })
+});
+</script>
 @endscript
