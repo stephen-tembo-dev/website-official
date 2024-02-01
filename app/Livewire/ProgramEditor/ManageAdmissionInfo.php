ee5440105b9ca7fa5242f9debabb32a064e1b82e
Stephen Tembo <stephen187dev@gmail.com>
2024-02-01 09:20:12 +0200

    manage admission info added

---
+++ app/Livewire/ProgramEditor/ManageAdmissionInfo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\ProgramEditor;
+
+use Livewire\Component;
+
+class ManageAdmissionInfo extends Component
+{
+    public function render()
+    {
+        return view('livewire.program-editor.manage-admission-info');
+    }
+}
---
+++ resources/views/livewire/program-editor/manage-admission-info.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <div class="container">
+        <table class="highlight striped responsive-table mt">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a href="/" class="btn btn-floating btn-small white"><i class="material-icons black-text">edit</i></a></td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+</div>
