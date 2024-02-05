f05a8e32f302859c7474a0af1804e9343ec0752f
Blessed Zulu <43051370+blessedzulu@users.noreply.github.com>
2024-02-05 08:25:48 +0200

    Updated /campus-life page

---
+++ resources/views/livewire/pages/campus-life.blade.php
@@ -0,0 +1,138 @@
+<div class="container">
+    <div class="row mt">
+        <div class="col s12">
+            <ul class="tabs mb-sm">
+                <li class="tab"><a href="#tab1">Campus Life</a></li>
+                <li class="tab"><a href="#tab2">Accommodation</a></li>
+                <li class="tab"><a href="#tab3">Activities</a></li>
+                <li class="tab"><a href="#tab4">Facilities</a></li>
+            </ul>
+            <div id="tab1" class="col s12 mb">
+                <div class="row mt mb">
+                    <div class="col s12 l6 center"><img src="{{ asset('images/placeholder.webp') }}"
+                            class="responsive-img">
+                    </div>
+
+                    <div class="col s12 l5 offset-l1">
+                        <h5 class="">Title 1</h5>
+                        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde ab
+                            eum
+                            corporis ratione enim neque veniam blanditiis natus laudantium consectetur quasi ex maiores
+                            perferendis laborum quidem, mollitia ipsum iusto!</p>
+
+                    </div>
+                </div>
+
+                <div class="row mt mb">
+                    <div class="col s12 l6 push-l6 center"><img src="{{ asset('images/placeholder.webp') }}"
+                            class="responsive-img">
+                    </div>
+
+                    <div class="col s12 l5 pull-l7 offset-l1">
+                        <h5 class="">Title 1</h5>
+                        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde ab
+                            eum
+                            corporis ratione enim neque veniam blanditiis natus laudantium consectetur quasi ex maiores
+                            perferendis laborum quidem, mollitia ipsum iusto!</p>
+
+                    </div>
+                </div>
+            </div>
+            <div id="tab2" class="col s12 mb">
+                <div class="row mt mb">
+                    <div class="col s12 l6 push-l6 center"><img src="{{ asset('images/placeholder.webp') }}"
+                            class="responsive-img">
+                    </div>
+
+                    <div class="col s12 l5 pull-l7 offset-l1">
+                        <h5 class="">Title 2</h5>
+                        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde ab
+                            eum
+                            corporis ratione enim neque veniam blanditiis natus laudantium consectetur quasi ex maiores
+                            perferendis laborum quidem, mollitia ipsum iusto!</p>
+
+                    </div>
+                </div>
+
+                <div class="row mt mb">
+                    <div class="col s12 l6 center"><img src="{{ asset('images/placeholder.webp') }}"
+                            class="responsive-img">
+                    </div>
+
+                    <div class="col s12 l5 offset-l1">
+                        <h5 class="">Title 2</h5>
+                        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde ab
+                            eum
+                            corporis ratione enim neque veniam blanditiis natus laudantium consectetur quasi ex maiores
+                            perferendis laborum quidem, mollitia ipsum iusto!</p>
+
+                    </div>
+                </div>
+            </div>
+            <div id="tab3" class="col s12 mb">
+                <div class="row mt mb">
+                    <div class="col s12 l6 center"><img src="{{ asset('images/placeholder.webp') }}"
+                            class="responsive-img">
+                    </div>
+
+                    <div class="col s12 l5 offset-l1">
+                        <h5 class="">Title 3</h5>
+                        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde ab
+                            eum
+                            corporis ratione enim neque veniam blanditiis natus laudantium consectetur quasi ex maiores
+                            perferendis laborum quidem, mollitia ipsum iusto!</p>
+
+                    </div>
+                </div>
+
+                <div class="row mt mb">
+                    <div class="col s12 l6 push-l6 center"><img src="{{ asset('images/placeholder.webp') }}"
+                            class="responsive-img">
+                    </div>
+
+                    <div class="col s12 l5 pull-l7 offset-l1">
+                        <h5 class="">Title 3</h5>
+                        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde ab
+                            eum
+                            corporis ratione enim neque veniam blanditiis natus laudantium consectetur quasi ex maiores
+                            perferendis laborum quidem, mollitia ipsum iusto!</p>
+
+                    </div>
+                </div>
+            </div>
+            <div id="tab4" class="col s12 mb">
+                <div class="row mt mb">
+                    <div class="col s12 l6 push-l6 center"><img src="{{ asset('images/placeholder.webp') }}"
+                            class="responsive-img">
+                    </div>
+
+                    <div class="col s12 l5 pull-l7 offset-l1">
+                        <h5 class="">Title 4</h5>
+                        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde ab
+                            eum
+                            corporis ratione enim neque veniam blanditiis natus laudantium consectetur quasi ex maiores
+                            perferendis laborum quidem, mollitia ipsum iusto!</p>
+
+                    </div>
+                </div>
+
+                <div class="row mt mb">
+                    <div class="col s12 l6 center"><img src="{{ asset('images/placeholder.webp') }}"
+                            class="responsive-img">
+                    </div>
+
+                    <div class="col s12 l5 offset-l1">
+                        <h5 class="">Title 4</h5>
+                        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde ab
+                            eum
+                            corporis ratione enim neque veniam blanditiis natus laudantium consectetur quasi ex maiores
+                            perferendis laborum quidem, mollitia ipsum iusto!</p>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
