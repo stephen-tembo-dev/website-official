2f45c199d382cc5633daeda2a86ed14b6eefab5f
Stephen Tembo <stephen187dev@gmail.com>
2024-02-05 11:44:27 +0200

    general admissions page added

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Middleware\CheckIfViewer;
 use App\Livewire\Program\Programs;
-use App\Livewire\Other\{Home, Contact, AboutUs, Dashboard};
+use App\Livewire\Other\{Home, Contact, AboutUs, Dashboard, CampusLife, GeneralAdmissionRequirements};
 use App\Livewire\ProgramEditor\{AddAdmissionInfo, EditAdmissionInfo, ManageAdmissionInfo};
 use App\Livewire\Events\{CreateEvent, EditEvent, ListEvents, ManageEvents, ShowEvent};
 use App\Livewire\News\{AddNewsStory, EditNewsStory, NewsStory, AllNewsStories, ManageNews};
 use App\Livewire\AboutEditor\{AddBannerContent, AddInfoListContent, AddMainContent, EditBannerContent, EditInfoListContent, EditMainContent};
 use App\Livewire\HomeEditor\{AddAnnouncement, AddSliderInfo, AddMarketingInfo, EditAnnouncement, EditSliderInfo, EditMarketingInfo, ManageSlider};
-use App\Livewire\Other\CampusLife;
+
 
 Route::get('/', Home::class);
 Route::get('/contact', Contact::class);
@@ -22,6 +22,7 @@ Route::get('/news-story/{news_id}', NewsStory::class);
 Route::get('/events', ListEvents::class)->name('events.index');
 Route::get('/event/{event}', ShowEvent::class)->name('events.show');
 Route::get('/campus-life', CampusLife::class)->name('campus-life');
+Route::get('/admission-requirements', GeneralAdmissionRequirements::class)->name('general-requirements');
 
 
 Route::group(['middleware' => ['auth', CheckIfViewer::class]], function () {
